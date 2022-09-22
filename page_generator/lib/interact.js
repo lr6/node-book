4749e3d8a346a558da00a610ad144a876efbedce
zhibo <36.zhibo@gmail.com>
2022-09-22 18:50:09 +0800

    tmp

--- page_generator/lib/interact.js
+++ page_generator/lib/interact.js
@@ -6,37 +6,26 @@
  * https://cloud.tencent.com/developer/article/1857433
  */
 
-// const os = require('os')
-// const platform = os.platform()
-
-
 import { platform } from 'os'
+import readline from 'readline'
+
 let suffix = -1
 if(platform() === 'win32') {
   suffix = -2
 }
 
-export function interact(questions) {
-  process.stdin.setEncoding('utf-8')
-
+function question(rl, { text, value }) {
+  const q = `${text}(${value})\r\n`
   return new Promise((resolve) => {
-    const answer = []
-    let i = 0
-    let { text, value } = questions[i++]
-    console.log(`${text}（默认值：${value}）`)
-    process.stdin.on('readable', () => {
-      const read = process.stdin.read()
-      let chunk = read.slice(0, suffix)
-      answer.push(chunk || value)
-      const nextQuestion = questions[i++]
-      if(nextQuestion) {
-        process.stdin.read()
-        text = nextQuestion.text
-        value = nextQuestion.value
-        console.log(`${text}（默认值：${value}）`)
-      } else {
-        resolve(answer)
-      }
+    rl.question(q, (answer) => {
+      resolve(answer || value)
     })
+  })
+}
+
+export function interact(questions) {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
   })
 }
