2af7c294c44de4ebabbb81c5d411131256fbf7c3
zhibo <36.zhibo@gmail.com>
2023-04-14 17:00:46 +0800

    服务端渲染

--- tcp/server.js
+++ tcp/server.js
@@ -1,9 +1,51 @@
 
 const Server = require('./http-interceptor.js')
 const Router = require('./middleware.js')
+const param = require('./param.js')
+const fs = require('fs')
 
 const router = new Router()
 const app = new Server()
+
+
+app.use(({ req }, next) => {
+    console.log(`${req.method} ${req.url}`)
+    next()
+})
+
+app.use(param)
+
+app.use(router.get('/cov/index', async ({ route, res }, next) => {
+    const { getCoronavirusKeyIndex } = require('./mock.js')
+    const index = getCoronavirusKeyIndex()
+    const handlebars = require('handlebars')
+
+    const tpl = fs.readFileSync('./coronavirus_index.html', { encoding: 'utf-8' })
+    const template = handlebars.compile(tpl)
+    const result = template({ data: index })
+    res.setHeader('Content-Type', 'text/html')
+    res.body = result
+    await next()
+}))
+
+app.use(router.get('/cov/:date', async ({ params, route, res }, next) => {
+    const { getCoronavirusByDate } = require('./mock.js')
+    const data = getCoronavirusByDate(route.date)
+
+    if (params.type === 'json') {
+        res.setHeader('Content-Type', 'application/json')
+        res.body = { data }
+    } else {
+        const handlebars = require('handlebars')
+        const tpl = fs.readFileSync('./coronavirus_date.html', { encoding: 'utf-8' })
+        const template = handlebars.compile(tpl)
+        const result = template({ data })
+        res.setHeader('Content-Type', 'text/html')
+        res.body = result
+    }
+
+    await next()
+}))
 
 app.use(router.all('/test/:course/:lecture', async ({ route, res }, next) => {
     res.setHeader('Content-Type', 'application/json')
@@ -13,7 +55,8 @@ app.use(router.all('/test/:course/:lecture', async ({ route, res }, next) => {
 
 app.use(router.all('.*', async ({ req, res }, next) => {
     res.setHeader('Content-Type', 'text/html')
-    res.body = '<h1>Hello world</h1>'
+    res.body = '<h1>not found</h1>'
+    res.statusCode = 404
     await next()
 }))
 
